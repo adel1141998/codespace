aef7940968015363c25ae934142f5a405db09f02
adel1141998 <119523157+adel1141998@users.noreply.github.com>
2023-03-13 16:06:36 +0000

    Mon, Mar 13, 2023, 4:06 PM +00:00

---
+++ hallo.c
@@ -0,0 +1,13 @@
+#include <stdoi.h>
+#include <cs50.h>
+int main (void){
+int count;
+do{
+    count = get_int("count :");
+}while(count < 1);
+
+ for(int i=0;i < count; i++ ){
+    printf("?");
+ }
+ printf("\n");
+}
